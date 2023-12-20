1b549be7002b4009e71d03bc98310750c44bc895
YanQi-2020 <qiyan0608@gmail.com>
2023-12-20 13:11:52 -0500

    admin panel data

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 
 class Menu(models.Model):
-    #ID = models.IntegerField()
+    ID = models.IntegerField(primary_key=True, max_length=6)
     Title = models.CharField(max_length=255)
     Price = models.DecimalField(max_digits=10, decimal_places=2)
     Inventory = models.IntegerField(max_length=5)
@@ -15,7 +15,7 @@ class Menu(models.Model):
         return self.Title
 
 class Booking(models.Model):
-    #ID = models.IntegerField()
+    ID = models.IntegerField(primary_key=True, max_length=11)
     Name = models.CharField(max_length=255)
     No_of_guests = models.IntegerField(max_length=6)
     BookingDate = models.DateTimeField()
