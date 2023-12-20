f347c9cd3e60ddab0764cefab079f79a07753a9c
YanQi-2020 <qiyan0608@gmail.com>
2023-12-20 14:12:54 -0500

    new environment venv

---
+++ venv/littlelemon/restaurant/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Menu
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = '__all__'  # You can specify specific fields if needed
